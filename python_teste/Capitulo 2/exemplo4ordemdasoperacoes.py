ce48125c31e3553cafabf7e9d384f2cdf20b8d5f
matheus camargo maciel <matheus.camargo.maciel@escola.pr.gov.br>
2025-05-09 09:41:44 -0300

    exemplo 4 ordem das operecoes

---
+++ python_teste/Capitulo 2/exemplo4ordemdasoperacoes.py
@@ -0,0 +1,16 @@
+#A maior prioridade é o parentese ()
+#Depois a Exponeciação / Potência
+#Depois Multiplicação e Divisão
+#Soma a Subtração na ordem que vier
+#exemplo
+num1 = 76
+num2 = 5
+num3 = 8
+num4 = 4
+num5 = 10
+num6 = 3
+num7 = 2
+
+total = num1 - (num2 + num3 / num4) * (num5 - num6 * num7)
+
+print(total)
