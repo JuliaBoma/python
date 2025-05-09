b16abe0e5bdc1bacfd210bd281fb94104487be80
matheus camargo maciel <matheus.camargo.maciel@escola.pr.gov.br>
2025-05-09 11:19:07 -0300

    exemplo 5 operacoes com strings

---
+++ python_teste/Capitulo 2/exemplo5operacoescomstrings.py
@@ -0,0 +1,11 @@
+#É proibido utilizar operações matematicas co strings
+#Ao menos em 2 casos:
+
+first = 'Jose'
+second = ' Valim'
+nomesobrenome = first + second # + siginifica concatenação
+print(nomesobrenome)
+
+StingQualquer = "Teste" * 3
+print(StingQualquer)
+
