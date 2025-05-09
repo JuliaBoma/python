bee0d01633eada5505bff997fdf15e22d12d3d0d
matheus camargo maciel <matheus.camargo.maciel@escola.pr.gov.br>
2025-05-09 10:56:25 -0300

    exemplos do capitulo1

---
+++ python_teste/Capitulo 1/exemplo3valoresetipos.py
@@ -0,0 +1,20 @@
+#Tipos de Dados
+#Inteiros 123456... Positivos e Negativos
+#Ponto Flutuante 1.0, 1,3, 5.685532..... Positivos e Negativos
+#Caracteres que são simbolos
+#Strings que são conjuntos de caracteres
+
+#Exemplo de uma variveç com Numero Inteiro
+num1 =7
+
+#Exemplo de uma variavel co numero de Ponto Flutuante
+num2 = 3.8156
+
+#Exemplo de uma variavel de um Caracter
+char1 = 'a'
+
+#Exemplo de uma variavel de uma String
+string1 = 'Julia'
+
+#Comando para descobrir qual o tipo da variavel
+print(type(num2))
