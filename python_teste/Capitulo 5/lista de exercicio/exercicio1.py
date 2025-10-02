abc5341a96b349c784c68f1d1eda6f945bd33462
Cauã <silva.caua27@escola.pr.gov.br>
2025-10-02 08:39:43 -0300

    lista de exercicio

---
+++ python_teste/Capitulo 5/lista de exercicio/exercicio1.py
@@ -0,0 +1,9 @@
+def colocarNome (nomeFilho, NomePai, NomeMae):
+    print(f"Nome do usuario {nomeFilho}")
+    print((f"Nome dos Pais {NomePai} e {NomeMae}"))
+
+nomeFilho = input("Qual é o seu nome: ")
+NomePai = input("Qual é o nome do seu Pai: ")
+NomeMae = input("Qual é o nome da sua Mãe: ")
+
+colocarNome(nomeFilho, NomePai, NomeMae)
---
+++ python_teste/Capitulo 5/lista de exercicio/exercicio2.py
@@ -0,0 +1,8 @@
+def somaNumero(n1, n2):
+    soma = n1 + n2
+    print(f"A resultado da soma é: {soma}")
+
+n1 = int(input("Digite um Número: "))
+n2 = int(input("Digite outro Número: "))
+
+somaNumero(n1,n2)
