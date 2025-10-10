59e03b690f522626e47e86c4d051db9a5d691909
Julia Boma <julia.boma.silva@escola.pr.gov.br>
2025-10-09 21:55:38 -0300

    continuação de exercicio2

--- python_teste/Capitulo 5/estrutura de decisão/exercicio7.py
+++ python_teste/Capitulo 5/estrutura de decisão/exercicio7.py
@@ -1,1 +1,11 @@
-n1 =
+def verificarnumero(abacaxi):
+    if abacaxi == 0:
+        print("E ZERO")
+    elif abacaxi > 0:
+        print("E positivo")
+    else:
+        print("E negativo")
+
+n1 = float(input("Digite um número "))
+
+verificarnumero(n1)
